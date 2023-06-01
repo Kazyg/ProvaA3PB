128ec415f8d2f6792b2e5f9b3b75d40510348fec
Tatihub1 <tatianeassis990104@gmail.com>
2023-06-01 11:21:32 -0300

    ModelEmprestimos

--- src/Model/Emprestimo.java
+++ src/Model/Emprestimo.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
 
-/**
- *
- * @author guilherme
- */
+import java.util.*;
+
 public class Emprestimo {
+    private int Id;
+    private String NomeAmigo;
+    private String NomeFerramenta;
+    private Date DataRetirada;
+    private Date DataDevolucao;
+    
+    public Emprestimo(){
+}
+    public Emprestimo (int Id, String NomeAmigo, String NomeFerramenta, Date DataRetirada, Date DataDevolucao){
+        this.Id = Id;
+        this.NomeAmigo = NomeAmigo;
+        this.NomeFerramenta = NomeFerramenta;
+        this.DataRetirada = DataRetirada;
+        this.DataDevolucao = DataDevolucao;
+}
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNomeAmigo() {
+        return NomeAmigo;
+    }
+
+    public void setNomeAmigo(String NomeAmigo) {
+        this.NomeAmigo = NomeAmigo;
+    }
+
+    public String getNomeFerramenta() {
+        return NomeFerramenta;
+    }
+
+    public void setNomeFerramenta(String NomeFerramenta) {
+        this.NomeFerramenta = NomeFerramenta;
+    }
+
+    public Date getDataRetirada() {
+        return DataRetirada;
+    }
+
+    public void setDataRetirada(Date DataRetirada) {
+        this.DataRetirada = DataRetirada;
+    }
+
+    public Date getDataDevolucao() {
+        return DataDevolucao;
+    }
+
+    public void setDataDevolucao(Date DataDevolucao) {
+        this.DataDevolucao = DataDevolucao;
+    }
+
+    
+
+            
     
 }

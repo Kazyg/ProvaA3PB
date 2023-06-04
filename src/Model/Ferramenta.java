77802b57e86641ce445ae2b69994f0af01f9d1bf
hillecammi <hillecammi2206@gmail.com>
2023-06-04 16:21:46 -0300

    Model ferramenta e model amigo

--- src/Model/Ferramenta.java
+++ src/Model/Ferramenta.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model;
 
-/**
- *
- * @author guilherme
- */
+package Model;
 public class Ferramenta {
-    
+    private String id;
+    private String nome;
+    private String marca;
+    private String custo;
+    // Construtores vazio 
+    public Ferramenta() {
+    }
+    // Construtores com atributos
+    public Ferramenta(String id, String nome, String marca, String custo) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.custo = custo;
+    }
+    // Construtores get e set
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCusto() {
+        return custo;
+    }
+
+    public void setCusto(String custo) {
+        this.custo = custo;
+    }
 }
